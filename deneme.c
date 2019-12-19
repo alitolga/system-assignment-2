9814eebbe9a88b9a796ab6685bea889bc171cfb4
alitolga <alitolga.dincer@gmail.com>
2019-12-19 16:20:13 +0300

    Set nice value implemented.

--- deneme.c
+++ deneme.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
-#define NR_mycall 351
+#include <sys/types.h>
+#include <unistd.h>
+
+/*
+#include "include/linux/syscalls.h"
+#include <linux/kernel.h>
+//#include <linux/sched.h> 	// find_task_by_vpid
+*/
+
+#define NR_mycall 352
 
 int main(void){
-	int x1=10,x2=5, y;
-	y = syscall(NR_mycall, x1, x2);
-	printf("y: %d", y);
+	pid_t pid = getpid();
+	printf("pid: %lu\n", pid);
+	long y;
+	int val = 10;
+	y = syscall(NR_mycall, pid, val);
+	printf("%ld\n", y);
+	
+	/*	
+	struct pid *pid_struct;
+	struct task_struct *t;
+	pid_struct = find_get_pid(pid);
+	t = pid_task(pid_struct, PIDTYPE_PID);
+	//t = find_task_by_vpid(pid);
+	printf("%d\n", (t->nice_inc) );
+	*/
 	
 	return 0;
 }
